3e8897735b1ec27cdfaceb26d97286a20e672d1b
Suresh <sureshgoudmula@gmail.com>
2026-04-07 21:30:36 +0530

    Done Design-1

--- Sample.java
+++ Sample.java
@@ -5,3 +5,37 @@
 
 
 // Your code here along with comments explaining your approach
+import java.util.Stack;
+
+class MinStack {
+    Stack<Integer> mainStack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        mainStack = new Stack<>();
+        minStack = new Stack<>();    
+    }
+    
+    public void push(int val) {
+        mainStack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) 
+            minStack.push(val);
+    }
+    
+    public void pop() {
+        int poppedValue = mainStack.pop();
+        if (poppedValue == minStack.peek()) 
+            minStack.pop();
+    }
+    
+    public int top() {
+        return mainStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+
+ 
